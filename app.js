44acbbfc6d1f91578cac2b4347f7fd160e176927
sunilchaudhary3112 <sunilchaudhary3112@gmail.com>
2020-09-10 15:28:32 +0530

    viber webhook version 1

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
+const express = require('express');
+const app = express();
+const axios = require('axios')
+
 const ViberBot = require('viber-bot').Bot;
 const BotEvents = require('viber-bot').Events;
 const PictureMessage = require('viber-bot').Message.Picture;
-
 const TextMessage = require('viber-bot').Message.Text;
-const express = require('express');
-const app = express();
+
+
 
 const port = process.env.PORT || 8080;
-// const webhookUrl = 'https://03d1d4e5186c.ngrok.io';
-const webhookUrl = 'https://safe-meadow-75391.herokuapp.com';
+const webhookUrl = 'https://81b0bce5d8b8.ngrok.io';
+// const webhookUrl = 'https://safe-meadow-75391.herokuapp.com';
 
+
+
+console.log('2');
 
 let init = () => {
     bot.setWebhook(webhookUrl)
+    console.log('3');
+
 };
 
 // Creating the bot with access token, name and avatar
@@ -27,21 +35,63 @@ const bot = new ViberBot({
         "message"]
 });
 
+
 app.use('/', bot.middleware());
 
+function say(response, message) {
+    response.send(new TextMessage(message));
+    console.log('4');
+
+}
+
+// bot.onSubscribe(response => {
+//     say(response, `Hi there you have subscribed to viber webhook`);
+//     console.log('5');
+
+// });
+console.log('4');
 // A simple regular expression to answer messages in the form of 'hi' and 'hello'.
 bot.onTextMessage(/^hi|hello$/i, (message, response) => {
-    response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}`));
-    // console.log('Got Request from Viber');
-    // console.log('------message------');
-    // console.log(message);
-    // console.log('------response------');
-    // console.log(response);
+    console.log('6');
+    try {
+        axios
+        .post('https://77b98ee9dfce.ngrok.io/sendToBot', {
+            text: "test"
+        })
+        .then(res => {
+            // console.log(`statusCode: ${res.status}`)
+            console.log(res.data.messagePayload.text);
+            // response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}`));
+            response.send(new TextMessage(`${res.data.messagePayload.text}`));
+
+        })
+        .catch(error => {
+            console.error(error);
+            response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}`));
+        })
+    } catch (error) {
+        console.log(error);
+    }
+    
+        // response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}`));
+
+});
+
+bot.onTextMessage(/./, (message, response) => {
+    console.log('8');
+
+    say(response, `This is message from bot.onTextMessage`);
 });
 
 
 const listener = app.listen(port, () => {
     console.log('Your Viber integration server is listening on port '
         + listener.address().port);
-    init();
+        try {
+            init();
+        } catch (error) {
+            console.log(error);
+        }
+    
+   
 });
